d3b5377b196f8833a7018ef30880f552f97f7b6e
Gabriel Marinho <gdcmarinho@gmail.com>
2022-07-24 00:52:04 -0300

    refactor: updates indentation

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -13,45 +13,45 @@ export class StlzdButton extends LitElement {
   static styles = [
     globalStyles,
     css`
-            button {
-                background-color: var(--_primary-color);
-                color: var(--_white-color);
-                border-radius: 4px;
-                font-family: "Inter", sans-serif;
-                font-size: 15px;
-                cursor: pointer;
-                outline: none;
-                border: none;
-                padding: 7px 20px;
-                transition: all 0.4s ease;
-            }
+      button {
+        background-color: var(--_primary-color);
+        color: var(--_white-color);
+        border-radius: 4px;
+        font-family: "Inter", sans-serif;
+        font-size: 15px;
+        cursor: pointer;
+        outline: none;
+        border: none;
+        padding: 7px 20px;
+        transition: all 0.4s ease;
+      }
 
-            button:hover {
-                background-color: var(--_primary-hovered-color);
-            }
+      button:hover {
+        background-color: var(--_primary-hovered-color);
+      }
 
-            button:active {
-                background-color: var(--_primary-active-color);
-            }
+      button:active {
+        background-color: var(--_primary-active-color);
+      }
 
-            :host([type="basic"]) button {
-                background-color: transparent;
-                color: var(--_primary-color);
-            }
+      :host([type="basic"]) button {
+        background-color: transparent;
+        color: var(--_primary-color);
+      }
 
-            :host([type="basic"]:hover) button {
-                background-color: var(--_lavender-blue-transparent-color);
-            }
+      :host([type="basic"]:hover) button {
+        background-color: var(--_lavender-blue-transparent-color);
+      }
 
-            :host([type="basic"]:active) button {
-                background-color: var(--_lavender-blue-transparent-color);
-            }
+      :host([type="basic"]:active) button {
+        background-color: var(--_lavender-blue-transparent-color);
+      }
 
-            :host([disabled]) button {
-              background-color: var(--_disabled-color);
-                cursor: default;
-            }
-        `,
+      :host([disabled]) button {
+        background-color: var(--_disabled-color);
+        cursor: default;
+      }
+    `,
   ];
 
   @property()
