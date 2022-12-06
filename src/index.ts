268319e0f74f9bddf7a2052cf0bfb5bb67e0b0d9
Gabriel Marinho <gdcmarinho@gmail.com>
2022-12-06 18:01:14 -0300

    feat: creates entrypoint file

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { StlzdAlert } from "./components/alert/alert";
+import { StlzdButton } from "./components/button/button";
+import { StlzdCommandLine } from "./components/command-line/command-line";
+import { StlzdHeader } from "./components/header/header";
+import { StlzdInput } from "./components/input/input";
+
+export { StlzdAlert, StlzdButton, StlzdCommandLine, StlzdHeader, StlzdInput };
